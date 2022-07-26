aaf62b88250863326f2cbf63b8d747308abc0d14
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2022-07-26 23:38:44 +0530

    Time: 73 ms (71.30%), Space: 30.1 MB (95.16%) - LeetHub

--- 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
@@ -4,14 +4,12 @@ public:
         vector<int> remFreq(k,0);
         int pre = 0;
         remFreq[0] = 1;
+        int count = 0;
         for(int i = 0 ; i < nums.size() ; i++){
             pre += nums[i];
             int rem = (k+pre%k)%k;
+            count += remFreq[rem];
             remFreq[rem]++;
-        }
-        int count = 0;
-        for(int i = 0 ; i < k ; i++){
-            count += (remFreq[i]*(remFreq[i]-1))/2;
         }
         return count;
     }
