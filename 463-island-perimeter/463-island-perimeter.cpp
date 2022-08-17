56a078e9bb038e63ed1c1ac76cee05b151ac3912
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2022-08-17 23:43:31 +0530

    Time: 187 ms (45.86%), Space: 96.2 MB (87.60%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.cpp
+++ 463-island-perimeter/463-island-perimeter.cpp
@@ -3,42 +3,21 @@ public:
     int islandPerimeter(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        queue<pair<int,int>> q;
-        vector<vector<int>> vis(m,vector<int>(n,0));
-        
+        int land = 0;
+        int dup = 0;
         for(int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
                 if(grid[i][j] == 1){
-                    q.push({i,j});
-                    vis[i][j] = 1;
-                    break;
+                    land++;
+                    if(i!=0 && grid[i-1][j] == 1)
+                        dup++;
+                    if(j!=0 && grid[i][j-1] == 1)
+                        dup++;
                 }
             }
-        }
-        int dir[] = {-1,0,1,0,-1};
-        int ans = 0;
-        while(!q.empty()){
-            auto [r,c] = q.front();
-            q.pop();
-            int curr = 4;
-            for(int i = 0 ; i < 4 ; i++){
-                int dr = r + dir[i];
-                int dc = c + dir[i+1];
-                if(dr<0 || dc<0 || dr>=m || dc>=n){
-                    continue;
-                }
-                if(grid[dr][dc] == 1){
-                    curr--;
-                    if(!vis[dr][dc]){
-                        q.push({dr,dc});
-                        vis[dr][dc] = 1;
-                    }
-                }
-            }
-            
-            ans += curr;
         }
         
-        return ans;
+        return land*4 - dup*2;
+        
     }
 };
