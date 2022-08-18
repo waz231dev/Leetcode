85e4ce8385667016a0e259df5d2bfaf59f10a654
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2022-08-19 01:15:18 +0530

    Time: 42 ms (9.26%), Space: 61.3 MB (5.05%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
-    bool isCycleHelper(vector<int> adj[],int src,vector<int>& vis){
-        queue<int> q;
-        q.push(src);
-    
+    bool isCycleHelper(vector<int> adj[],int src,vector<int> vis){
+        queue<pair<int,int>> q;
+        q.push({src,-1});
+        vis[src] = 1;
         while(!q.empty()){
-            int node = q.front();
+            int node = q.front().first;
+            int parent = q.front().second;
             q.pop();
             
-            if(vis[node])
-                return true;
-            
-            vis[node] = 1;
             for(auto it : adj[node]){
                 if(!vis[it]){
-                    q.push(it);
+                    q.push({it,node});
+                    vis[it] = 1;
+                }else if(parent != it){
+                    return true;
                 }
             }
             
-        }
-        
-        return false;
-    }
-    bool isCycle(int n,vector<int> adj[]){
-        vector<int> vis(n+1,0);
-        for(int i = 1 ; i <= n ; i++){
-            if(!vis[i]){
-                if(isCycleHelper(adj,i,vis))
-                    return true;
-            }
         }
         return false;
     }
@@ -39,7 +28,10 @@ public:
         for(int i = 0 ; i < n ; i++){
             adj[edges[i][0]].push_back(edges[i][1]);
             adj[edges[i][1]].push_back(edges[i][0]);
-            if(isCycle(n,adj))
+            
+            // print(adj,n);
+            vector<int> vis(n+1,0);
+            if(isCycleHelper(adj,edges[i][0],vis))
                 return edges[i];
             // cout<<isCycle(n,adj);
             // isCycle(n,adj);
@@ -47,4 +39,23 @@ public:
         
         return {};
     }
+    
+    void print(vector<int> adj[],int n){
+        for(int i = 0 ; i <= n ; i++){
+            for(int j = 0 ; j < adj[i].size() ; j++){
+                cout<<i<<"-"<<adj[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
 };
+    // bool isCycle(int n,vector<int> adj[]){
+    //     vector<int> vis(n+1,0);
+    //     for(int i = 1 ; i <= n ; i++){
+    //         if(!vis[i]){
+    //             if(isCycleHelper(adj,i,vis))
+    //                 return true;
+    //         }
+    //     }
+    //     return false;
+    // }
