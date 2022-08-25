6fb2f4c7744cad6a75a5b790e6fca022a44fba6b
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2022-08-25 11:45:07 +0530

    Time: 64 ms (99.56%), Space: 27.1 MB (99.84%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+/*    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
         // vector<vector<int>> ans(m,vector<int>(n,0));
@@ -30,6 +30,34 @@ public:
                     vis[nr][nc] = 1;
                     mat[nr][nc] = d+1;
                     q.push({nr,nc,d+1});
+                }
+            }
+        }
+        return mat;
+    }
+    */
+    
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat){
+        int m = mat.size();
+        int n = mat[0].size();
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(mat[i][j] != 0){
+                    int top = 1e5,left = 1e5;
+                    if(i>0) top = mat[i-1][j];
+                    if(j>0) left = mat[i][j-1];
+                    mat[i][j] = min(top,left)+1;
+                }
+            }
+        }
+        
+        for(int i = m-1 ; i >= 0 ; i--){
+            for(int j = n-1 ; j >= 0 ; j--){
+                if(mat[i][j] != 0){
+                    int bottom = 1e5,right = 1e5;
+                    if(i<m-1) bottom = mat[i+1][j];
+                    if(j<n-1) right = mat[i][j+1];
+                    mat[i][j] = min(mat[i][j],min(bottom,right)+1);
                 }
             }
         }
