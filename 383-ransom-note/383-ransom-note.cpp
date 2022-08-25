42c3cfba364ad86c012484d82a15d149482c7f9c
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2022-08-25 10:55:36 +0530

    Time: 7 ms (98.98%), Space: 8.7 MB (89.81%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int freq[26] = {0};
+        for(char c : magazine){
+            freq[c-'a']++;
+        }
+        for(char c : ransomNote){
+            freq[c-'a']--;
+        }
+        
+        for(int i = 0 ; i < 26 ; i++){
+            if(freq[i]<0)
+                return false;
+        }
+        
+        return true;
+    }
+};
