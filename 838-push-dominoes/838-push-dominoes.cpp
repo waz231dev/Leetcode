54084d56d86e4a99092f6fc0e0e6cb1156366013
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2022-09-28 00:19:22 +0530

    Time: 64 ms (59.79%), Space: 12.7 MB (94.28%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.cpp
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -1,21 +1,44 @@
 class Solution {
 public:
-    string pushDominoes(string d) {
-        d = 'L' + d + 'R';
-        string res = "";
-        for (int i = 0, j = 1; j < d.length(); ++j) {
-            if (d[j] == '.') continue;
-            int middle = j - i - 1;
-            if (i > 0)
-                res += d[i];
-            if (d[i] == d[j])
-                res += string(middle, d[i]);
-            else if (d[i] == 'L' && d[j] == 'R')
-                res += string(middle, '.');
-            else
-                res += string(middle / 2, 'R') + string(middle % 2, '.') + string(middle / 2, 'L');
-            i = j;
+    string pushDominoes(string dom) {
+        int l=-1, r=-1, len=dom.length(), tmp;
+        
+        for(int i=0; i<len; i++){
+            
+            if(l==-1 && dom[i]!='.'){
+                if(dom[i]=='L'){
+                    for(int j=0; j<i; j++) dom[j]='L';
+                    l=i;
+                }
+                else {l=i;}
+            }
+            else if(dom[i]!='.'){
+                if(dom[l]=='L' && dom[i]=='L'){
+                    for(int j=l; j<i; j++) dom[j]='L';
+                    l=i;
+                }
+                else if(dom[l]=='R' && dom[i]=='R'){
+                    for(int j=l; j<i; j++) dom[j]='R';
+                    l=i;
+                }
+                else if(dom[l]=='L' && dom[i]=='R'){
+                    l=i;
+                }
+                else{ // R - L
+                    r=i;
+                    while(l<r){
+                        dom[l++]='R';
+                        dom[r--]='L';
+                    }
+                    l=i;
+                }
+            } 
         }
-        return res;
+        
+        if(l<len && l!=-1 && dom[l]=='R'){
+            while(l<len) dom[l++]='R';
+        }
+
+        return dom;
     }
 };
