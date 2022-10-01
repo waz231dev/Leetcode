5a0c39e3ac1dc3976fea1e3739db6dac219fcc5e
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2022-10-01 13:49:49 +0530

    Time: 14 ms (33.23%), Space: 8.7 MB (78.93%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,0));
+        vector<vector<int>> dp(2,vector<int>(n+1,0));
         
         for(int i = n - 1 ; i >= 0 ; i--){
             for(int j = 0 ; j <= i ; j++){
                 if(i == n-1){
-                    dp[i][j] = triangle[i][j];
+                    dp[i&1][j] = triangle[i][j];
                 }else{
-                    int down = dp[i+1][j];
-                    int diag = dp[i+1][j+1];
-                    dp[i][j] = min(down,diag) + triangle[i][j];
+                    int down = dp[(i+1)&1][j];
+                    int diag = dp[(i+1)&1][j+1];
+                    dp[i&1][j] = min(down,diag) + triangle[i][j];
                 }
             }
         }
