468d2cca9079761b819f82d274826d75c443f308
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2022-10-01 11:46:37 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (49.09%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,22 +1,33 @@
 class Solution {
     int solve(string& s,int ind,vector<int>& dp){
-        if(ind == s.size())
+        if(ind < 0)
             return 1;
-        if(ind > s.size() || s[ind] == '0')
+        if(s[ind] == '0')
             return 0;
         if(dp[ind] != -1) return dp[ind];
-        int left = solve(s,ind+1,dp);
+        int left = solve(s,ind-1,dp);
         int right = 0;
-        
-        if(stoi(s.substr(ind,2)) >= 1 && stoi(s.substr(ind,2)) <= 26){
-            right = solve(s,ind+2,dp);
+        if(ind >= 1 &&(s[ind-1] == '1' || (s[ind-1] == '2' && s[ind] <= '6'))){
+            right = solve(s,ind-2,dp);
         }
-        
+        // cout<<left<<endl;
         return dp[ind] = left+right;
     }
 public:
     int numDecodings(string s) {
-        vector<int> dp(s.size(),-1);
-        return solve(s,0,dp);    
+        int n = s.size();
+        vector<int> dp(n+1,0);
+        dp[n] = 1;
+        for(int i = n-1 ; i >= 0 ; i--){
+            if(s[i] == '0') dp[i] = 0;
+            else{ 
+                dp[i] = dp[i+1];
+                if(i < n-1 && (s[i] == '1' || (s[i] == '2' && s[i+1] <= '6'))){
+                    dp[i] += dp[i+2];
+                }
+            }
+        }
+        return dp[0];
+        
     }
 };
