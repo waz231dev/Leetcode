2f2f3b8f6b1c17a3a551394342841e228b533008
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2022-10-08 13:28:11 +0530

    Time: 45 ms (43.90%), Space: 6.4 MB (95.27%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,25 +1,39 @@
 class Solution {
-    int solve(int i,int j,string& t1,string& t2,vector<vector<int>>& dp){
-        if(i < 0 || j < 0) return 0;
-        if(i == 0 && j == 0){
-            return t1[i] == t2[j];
-        }
-        if(dp[i][j] != -1) return dp[i][j];
+//     int solve(int i,int j,string& t1,string& t2,vector<vector<int>>& dp){
+//         if(i < 0 || j < 0) return 0;
+//         if(i == 0 && j == 0){
+//             return t1[i] == t2[j];
+//         }
+//         if(dp[i][j] != -1) return dp[i][j];
         
-        if(t1[i] == t2[j]){
-            return dp[i][j] = 1+solve(i-1,j-1,t1,t2,dp);
-        }
+//         if(t1[i] == t2[j]){
+//             return dp[i][j] = 1+solve(i-1,j-1,t1,t2,dp);
+//         }
         
-        int one = solve(i-1,j,t1,t2,dp);
-        int two = solve(i,j-1,t1,t2,dp);
-        return dp[i][j] = max(one,two);
-    }
+//         int one = solve(i-1,j,t1,t2,dp);
+//         int two = solve(i,j-1,t1,t2,dp);
+//         return dp[i][j] = max(one,two);
+//     }
 public:
-    int longestCommonSubsequence(string text1, string text2) {
+    int longestCommonSubsequence(string s1, string s2) {
         
-        int m = text1.size();
-        int n = text2.size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return solve(m-1,n-1,text1,text2,dp);
+        int m = s1.size();
+        int n = s2.size();
+        vector<int> prev(n),curr(n);
+        
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(s1[i] == s2[j]){
+                    curr[j] = 1;
+                    if(i > 0 && j > 0)
+                        curr[j] += prev[j-1];
+                }else{
+                    if(i > 0) curr[j] = max(curr[j],prev[j]);
+                    if(j > 0) curr[j] = max(curr[j],curr[j-1]);
+                }
+            }
+            prev = curr;
+        }
+        return prev[n-1];
     }
 };
