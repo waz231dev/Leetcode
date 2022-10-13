ccedb8f8d525a78db71b97cd1ea182b576e0e384
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2022-10-13 11:47:32 +0530

    Time: 15 ms (77.98%), Space: 9 MB (60.15%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,4 +1,5 @@
 class Solution {
+    /*Memoization
     int solve(int i,int j,string& s1,string& s2,vector<vector<int>>& dp){
         if(i == 0) return j;
         if(j == 0) return i;
@@ -16,12 +17,34 @@ class Solution {
         
         return dp[i][j] = min(insert,min(replace,remove));
     }
+    */
 public:
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
+        //Tabulation
+        vector<vector<int>> dp(n+1,vector<int>(m+1));
         
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return solve(n,m,word1,word2,dp);
+        for(int j = 0 ; j <= m ; j++){
+            dp[0][j] = j;
+        }
+        
+        for(int i = 0 ; i <= n ; i++){
+            dp[i][0] = i;
+        }
+        
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= m ; j++){
+                if(word1[i-1] == word2[j-1]) dp[i][j] = dp[i-1][j-1];
+                else{
+                    int insert = 1 + dp[i][j-1];
+                    int replace = 1 + dp[i-1][j-1];
+                    int remove = 1 + dp[i-1][j];
+
+                    dp[i][j] = min(insert,min(replace,remove));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
