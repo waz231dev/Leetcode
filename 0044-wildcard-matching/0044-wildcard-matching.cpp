47b312bb68239b95a275035ad8436315c639010d
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2022-10-13 13:17:27 +0530

    Time: 67 ms (74.72%), Space: 28 MB (15.19%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,20 +1,20 @@
 class Solution {
     bool solve(int i, int j,string& s,string& p,vector<vector<int>>& dp){
         
-        if(i < 0 && j < 0) return true;
-        if(j < 0 && i >= 0) return false;
+        if(i == 0 && j == 0) return true;
+        if(j == 0 && i > 0) return false;
         
-        if(i < 0 && j >= 0){
-            for(int it = 0 ; it <= j ; it++){
+        if(i == 0 && j > 0){
+            for(int it = 0 ; it < j ; it++){
                 if(p[it] != '*') return false;
             }
             return true;
         }
         if(dp[i][j] != -1) return dp[i][j];
-        if(s[i] == p[j] || p[j] == '?')
+        if(s[i-1] == p[j-1] || p[j-1] == '?')
             return dp[i][j] = solve(i-1,j-1,s,p,dp);
         
-        if(p[j] == '*'){
+        if(p[j-1] == '*'){
             return dp[i][j] = solve(i-1,j,s,p,dp) || solve(i,j-1,s,p,dp);
         }
         
@@ -25,6 +25,6 @@ public:
         int n = s.size();
         int m = p.size();
         vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return solve(n-1,m-1,s,p,dp);
+        return solve(n,m,s,p,dp);
     }
 };
