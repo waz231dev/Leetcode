7c66a8775904ae73e48d5ddf6d84dbc4369d871a
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2022-10-13 11:39:48 +0530

    Time: 21 ms (60.04%), Space: 9 MB (60.15%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,12 +1,12 @@
 class Solution {
     int solve(int i,int j,string& s1,string& s2,vector<vector<int>>& dp){
-        if(i < 0) return j + 1;
-        if(j < 0) return i + 1;
+        if(i == 0) return j;
+        if(j == 0) return i;
         
         if(dp[i][j] != -1) return dp[i][j];
         
         //Matched
-        if(s1[i] == s2[j]) return solve(i-1,j-1,s1,s2,dp);
+        if(s1[i-1] == s2[j-1]) return solve(i-1,j-1,s1,s2,dp);
         
         //Not Matched
         int insert = 1 + solve(i,j-1,s1,s2,dp);
@@ -21,7 +21,7 @@ public:
         int n = word1.size();
         int m = word2.size();
         
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,word1,word2,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return solve(n,m,word1,word2,dp);
     }
 };
