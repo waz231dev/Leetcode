2ba55b23bc7f374fa736ab1f7281d62689925d3b
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2022-10-13 11:55:10 +0530

    Time: 24 ms (52.62%), Space: 6.5 MB (90.52%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -23,28 +23,30 @@ public:
         int n = word1.size();
         int m = word2.size();
         //Tabulation
-        vector<vector<int>> dp(n+1,vector<int>(m+1));
+        vector<int> prev(m+1),curr(m+1);
         
         for(int j = 0 ; j <= m ; j++){
-            dp[0][j] = j;
+            prev[j] = j;
         }
         
-        for(int i = 0 ; i <= n ; i++){
-            dp[i][0] = i;
-        }
+        // for(int i = 0 ; i <= n ; i++){
+        //     curr[0] = i;
+        // }
         
         for(int i = 1 ; i <= n ; i++){
+            curr[0] = i;
             for(int j = 1 ; j <= m ; j++){
-                if(word1[i-1] == word2[j-1]) dp[i][j] = dp[i-1][j-1];
+                if(word1[i-1] == word2[j-1]) curr[j] = prev[j-1];
                 else{
-                    int insert = 1 + dp[i][j-1];
-                    int replace = 1 + dp[i-1][j-1];
-                    int remove = 1 + dp[i-1][j];
+                    int insert = 1 + curr[j-1];
+                    int replace = 1 + prev[j-1];
+                    int remove = 1 + prev[j];
 
-                    dp[i][j] = min(insert,min(replace,remove));
+                    curr[j] = min(insert,min(replace,remove));
                 }
             }
+            prev = curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
