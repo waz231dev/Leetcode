d399ab501c43c954ec1ae5481ec4fab76556efec
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2022-10-13 13:30:39 +0530

    Time: 149 ms (28.74%), Space: 27.7 MB (24.56%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -24,7 +24,28 @@ public:
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return solve(n,m,s,p,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1));
+        
+        //Base Case
+        dp[0][0] = 1;
+        for(int j = 1 ; j <= m ; j++){
+            dp[0][j] = 1;
+            for(int it = 0 ; it < j ; it++){
+                if(p[it] != '*') dp[0][j] = 0;
+            }
+        }
+        
+        for(int i = 1; i <= n ; i++){
+            for(int j = 1 ; j <= m ; j++){
+                if(s[i-1] == p[j-1] || p[j-1] == '?')
+                    dp[i][j] = dp[i-1][j-1];
+                else if(p[j-1] == '*')
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                else 
+                    dp[i][j] = false;
+            }
+            
+        }
+        return dp[n][m];
     }
 };
