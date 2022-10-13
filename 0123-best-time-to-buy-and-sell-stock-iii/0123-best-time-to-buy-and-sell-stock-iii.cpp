18d400f5242fb2aa2a4d91e1a90728a0809569a1
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2022-10-14 00:49:19 +0530

    Time: 1556 ms (15.33%), Space: 207.1 MB (30.83%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -18,7 +18,25 @@ class Solution {
     }
 public:
     int maxProfit(vector<int>& prices) {
-        vector<vector<vector<int>>> dp(prices.size(),vector<vector<int>>(2,vector<int>(3,-1)));
-        return solve(0,1,2,prices,dp);
+        vector<vector<vector<int>>> dp(prices.size()+1,vector<vector<int>>(2,vector<int>(3,0)));
+        
+        for(int day = prices.size()-1 ; day >= 0 ; day--){
+            for(int canBuy = 0 ; canBuy <= 1 ; canBuy++){
+                for(int trans = 1 ; trans <= 2 ; trans++){
+                    int profit = 0;
+                    if(canBuy){
+                        int buy = -prices[day] + dp[day+1][0][trans];
+                        int notBuy = dp[day+1][1][trans];
+                        profit = max(buy,notBuy);
+                    }else{
+                        int sell = prices[day] + dp[day+1][1][trans-1];
+                        int notSell = dp[day+1][0][trans];
+                        profit = max(sell,notSell);
+                    }
+                    dp[day][canBuy][trans] = profit;
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 };
