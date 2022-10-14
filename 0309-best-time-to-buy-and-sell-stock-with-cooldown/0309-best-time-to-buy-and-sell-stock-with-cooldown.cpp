e8204037f92fc3100c2736c44b3e12d1c8cc5c0f
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2022-10-14 11:49:51 +0530

    Time: 13 ms (27.80%), Space: 11.2 MB (90.94%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -20,22 +20,22 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        vector<vector<int>> dp(3,vector<int>(2,0));
         
         for(int ind = n-1 ; ind >= 0 ; ind--){
             for(int canBuy = 0 ; canBuy <= 1 ; canBuy++){
                 int profit = 0;
                 if(canBuy){
-                    int buy = -prices[ind] + dp[ind+1][0];
-                    int notBuy = dp[ind+1][1];
+                    int buy = -prices[ind] + dp[(ind+1)%3][0];
+                    int notBuy = dp[(ind+1)%3][1];
                     profit = max(buy,notBuy);
                 }else{
                     int sell = prices[ind];
-                    if(ind+2 <= n) sell += dp[ind+2][1];
-                    int notSell = dp[ind+1][0];
+                    if(ind+2 <= n) sell += dp[(ind+2)%3][1];
+                    int notSell = dp[(ind+1)%3][0];
                     profit = max(sell,notSell);
                 }
-                dp[ind][canBuy] = profit;
+                dp[ind%3][canBuy] = profit;
             }
         }
         return dp[0][1];
