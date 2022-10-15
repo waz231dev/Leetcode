efc90d63cf19ca66158089c7a2ee0499d3e9f819
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2022-10-15 12:32:03 +0530

    Time: 12 ms (91.22%), Space: 10.4 MB (89.39%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -11,20 +11,35 @@ class Solution {
 //         return dp[ind][prev_ind+1] = max(take,notTake);
 //     }
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(2,vector<int>(n+1,0));
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<vector<int>> dp(2,vector<int>(n+1,0));
         
-        for(int ind = n-1 ; ind >= 0 ; ind--){
-            for(int prev_ind = ind-1 ; prev_ind >= -1 ; prev_ind--){
-                int notTake = dp[(ind+1)%2][prev_ind+1];
-                int take = 0;
-                if(prev_ind == -1 || nums[ind] > nums[prev_ind])
-                    take = 1 + dp[(ind+1)%2][ind+1];
+//         for(int ind = n-1 ; ind >= 0 ; ind--){
+//             for(int prev_ind = ind-1 ; prev_ind >= -1 ; prev_ind--){
+//                 int notTake = dp[(ind+1)%2][prev_ind+1];
+//                 int take = 0;
+//                 if(prev_ind == -1 || nums[ind] > nums[prev_ind])
+//                     take = 1 + dp[(ind+1)%2][ind+1];
 
-                dp[ind%2][prev_ind+1] = max(take,notTake);
-            }
+//                 dp[ind%2][prev_ind+1] = max(take,notTake);
+//             }
+//         }
+//         return dp[0][0];
+//     }
+    
+    int lengthOfLIS(vector<int>& nums) {
+        
+        vector<int> lis;
+        lis.push_back(nums[0]);
+        for(int i = 1 ; i < nums.size() ; i++){
+            auto ind = lower_bound(lis.begin(),lis.end(),nums[i])-lis.begin();
+            if(ind == lis.size())
+                lis.push_back(nums[i]);
+            else
+                lis[ind] = nums[i];
         }
-        return dp[0][0];
+        
+        return lis.size();
     }
 };
