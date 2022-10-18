2cc6423b2bacc63b8f51d9bdc366b227d2b763ee
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2022-10-19 00:19:55 +0530

    Time: 19 ms (43.50%), Space: 6.5 MB (89.76%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,26 @@
+class Solution {
+//     void solve(int n,string asf){
+//         if(n == 1){
+//             // return "1";
+//         }
+        
+        
+//     }
+public:
+    string countAndSay(int n) {
+        if(n == 1) return "1";
+        
+        string asf = countAndSay(n-1);
+        string ans = "";
+        for(int i = 0 ; i < asf.size() ;){
+            int count = 1;
+            int j;
+            for(j = i+1 ; j < asf.size() && asf[i] == asf[j] ; j++) count++;
+            ans += to_string(count);
+            ans += asf[i];
+            i = j;
+        }
+        
+        return ans;
+    }
+};
