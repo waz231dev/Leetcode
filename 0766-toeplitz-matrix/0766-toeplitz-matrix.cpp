147f6c3e359d3bf9287cb15441760be396b051a6
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2022-10-31 11:21:12 +0530

    Time: 27 ms (66.19%), Space: 17.5 MB (15.16%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -3,18 +3,18 @@ public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        unordered_map<int,int> mp;
-        for(int i = 0 ; i < m ; i++){
-            mp[i] = matrix[i][0];
-        }
+//         unordered_map<int,int> mp;
+//         for(int i = 0 ; i < m ; i++){
+//             mp[i] = matrix[i][0];
+//         }
         
-        for(int j = 1 ; j < n ; j++){
-            mp[-j] = matrix[0][j];
-        }
+//         for(int j = 1 ; j < n ; j++){
+//             mp[-j] = matrix[0][j];
+//         }
         
         for(int i = 1 ; i < m ; i++){
             for(int j = 1 ; j < n ; j++){
-                if(matrix[i][j] != mp[i-j]) return false;
+                if(matrix[i][j] != matrix[i-1][j-1]) return false;
             }
         }
         
