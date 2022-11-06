82c7b8789015e637700f15a5414a9039455308cf
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2022-11-06 19:01:26 +0530

    Time: 5 ms (75.86%), Space: 12 MB (12.07%) - LeetHub

---
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string orderlyQueue(string s, int k) {
+        
+        if(k > 1){
+            sort(s.begin(),s.end());
+            return s;
+        }
+        
+        string ans = s;
+        for(int i = 0 ; i < s.size() ; i++){
+            s = s.substr(1) + s[0];
+            ans = min(ans,s);
+        }
+        
+        return ans;
+    }
+};
