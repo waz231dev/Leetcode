c9cf30aa421207c9aaa21a56b446995970730f91
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2022-11-07 10:30:32 +0530

    Time: 3 ms (38.22%), Space: 6.2 MB (8.65%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        string s = to_string(num);
-        for(int i = 0 ; i < s.size() ; i++){
-            if(s[i] == '6'){
-                s[i] = '9';
-                break;
-            }
+//         string s = to_string(num);
+//         for(int i = 0 ; i < s.size() ; i++){
+//             if(s[i] == '6'){
+//                 s[i] = '9';
+//                 break;
+//             }
+//         }
+        
+//         return stoi(s);
+        
+        int temp = num;
+        int idx = -1;
+        int i = 0;
+        while(temp){
+            if(temp%10 == 6) idx = i;
+            temp /= 10;
+            i++;
         }
         
-        return stoi(s);
+        if(idx == -1) return num;
+        return num + 3*(pow(10,idx));
     }
 };
