ae564e4af00bc7dfb94a00f9962801d1086c0933
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2023-03-17 13:13:12 +0530

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.cpp
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -4,24 +4,35 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-    bool check(int start,vector<int> adj[],vector<int>& color){
-        queue<int> q;
-	    q.push(start);
-	    color[start] = 0;
-	    while(!q.empty()){
-	        int node = q.front();
-	        q.pop();
-	        int col = color[node];
-	        for(auto adjNode : adj[node]){
-	            if(color[adjNode] == -1){
-	                q.push(adjNode);
-	                color[adjNode] = col ^ 1;
-	            }else{
-	                if(color[adjNode] == col) return false;
-	            }
-	        }
-	    }
-	        return true;
+    // bool check(int start,vector<int> adj[],vector<int>& color){
+    //     queue<int> q;
+	   // q.push(start);
+	   // color[start] = 0;
+	   // while(!q.empty()){
+	   //     int node = q.front();
+	   //     q.pop();
+	   //     int col = color[node];
+	   //     for(auto adjNode : adj[node]){
+	   //         if(color[adjNode] == -1){
+	   //             q.push(adjNode);
+	   //             color[adjNode] = col ^ 1;
+	   //         }else{
+	   //             if(color[adjNode] == col) return false;
+	   //         }
+	   //     }
+	   // }
+	   //     return true;
+    // }
+    bool dfs(int node,vector<int> adj[],vector<int>& colors,int color){
+        colors[node] = color;
+        for(auto adjNode : adj[node]){
+            if(colors[adjNode] == -1){
+                if(!dfs(adjNode,adj,colors,!color)) return false;
+            }else{
+                if(colors[adjNode] == colors[node]) return false;
+            }
+        }
+        return true;
     }
 public:
 	bool isBipartite(int v, vector<int>adj[]){
@@ -29,7 +40,7 @@ public:
 	    vector<int> color(v,-1);
 	    for(int i = 0 ; i < v ; i++){
 	        if(color[i] == -1)
-	            if(!check(i,adj,color)) return false;
+	            if(!dfs(i,adj,color,0)) return false;
 	    }
 	    return true;
 	}
