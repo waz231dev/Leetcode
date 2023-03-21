bb66d5be10ce1be66cb215394aa774bbd873c2fd
Wasim Akram <83710321+waz231dev@users.noreply.github.com>
2023-03-21 14:33:20 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -5,25 +5,25 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   private:
-//   bool dfsCheck(int node, vector<int> adj[],int vis[],int pathVis[]){
-//       vis[node] = 1;
-//       pathVis[node] = 1;
+  /*bool dfsCheck(int node, vector<int> adj[],int vis[],int pathVis[]){
+      vis[node] = 1;
+      pathVis[node] = 1;
        
        
-//       for(auto adjNode : adj[node]){
-//           if(!vis[adjNode]){
-//               if(dfsCheck(adjNode,adj,vis,pathVis)) return true;
-//           }
-//             else if(pathVis[adjNode]){
-//                 return true;
-//             }
-//       }
+      for(auto adjNode : adj[node]){
+          if(!vis[adjNode]){
+              if(dfsCheck(adjNode,adj,vis,pathVis)) return true;
+          }
+            else if(pathVis[adjNode]){
+                return true;
+            }
+      }
        
        
        
-//       pathVis[node] = 0;
-//       return false;
-//   }
+      pathVis[node] = 0;
+      return false;
+  }
     bool dfsCheck(int node, vector<int> adj[],int vis[]){
        vis[node] = 2;
        
@@ -40,20 +40,47 @@ class Solution {
        
        vis[node] = 1;
        return false;
-   }
+   }*/
   public:
     // Function to detect cycle in a directed graph.
     bool isCyclic(int V, vector<int> adj[]) {
         // code here
-        int vis[V] = {0};
+        /*int vis[V] = {0};
         // int pathVis[V] = {0};
         for(int i = 0 ; i < V ; i++){
             if(!vis[i]){
                 if(dfsCheck(i,adj,vis)) return true;
             }
-        }
+        }*/
         
-        return false;
+        queue<int> q;
+	    int indegree[V]={0};
+	    vector<int> ans;
+	    for(int i = 0 ; i < V ; i++){
+	        for(int j = 0 ; j < adj[i].size() ; j++){
+	            indegree[adj[i][j]]++;
+	        }
+	    }
+	    
+	    for(int i = 0 ; i < V ; i++){
+	        if(!indegree[i]){
+	            q.push(i);
+	            ans.push_back(i);
+	        }
+	    }
+	    while(!q.empty()){
+	        int node = q.front();
+	        q.pop();
+	        
+	        for(auto it : adj[node]){
+	            indegree[it]--;
+	            if(indegree[it] == 0){
+	                q.push(it);
+	                ans.push_back(it);
+	            }
+	        }
+	    }
+	    return ans.size()<V;
     }
 };
 
